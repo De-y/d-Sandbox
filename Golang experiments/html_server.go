b1684997f71d15e7d22ad8bf0ba3ff39598f9e5e
Defy <61808223+De-y@users.noreply.github.com>
2023-04-02 11:42:34 -0700

    Experimentational Progress

--- Golang experiments/html_server.go
+++ Golang experiments/html_server.go
@@ -1,37 +1,82 @@
-// Make a HTML Http Server that renders HTML files and serves them, as well as static files.
+// Make a simple GO web server that serves the html files from  /templates, static files from /static, and the .ico file from /static. Also make a login function using prisma-client-go@latest.
 
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"path/filepath"
-	"strings"
+	"os"
+
+	"github.com/gorilla/mux"
+
+	"github.com/prisma/prisma-client-go"
 )
 
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/favicon.ico", faviconHandler)
-	http.HandleFunc("/static/", staticHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Create a new router
+	r := mux.NewRouter()
+
+	// Serve static files
+	fs := http.FileServer(http.Dir("./static"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+
+	// Serve favicon
+	favicon := http.FileServer(http.Dir("./static"))
+	r.PathPrefix("/favicon.ico").Handler(http.StripPrefix("/favicon.ico", favicon))
+
+	// Serve html files
+	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/login", loginHandler)
+
+	// Start the server
+	fmt.Println("Server is listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/index.html")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a new Prisma Client
+	client := prisma.New(nil)
+
+	// Get the email and password from the form
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	// Find the user in the database
+	user, err := client.User.FindUnique(prisma.User.Email.Equals(email)).Exec(context.Background())
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 	}
-	t.Execute(w, nil)
+
+	// Check if the password is correct
+	if user.Password == password {
+		fmt.Println("Logged in")
+	} else {
+		fmt.Println("Wrong password")
+	}
 }
 
-func faviconHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "static/favicon.ico")
-}
+// Path: index.html
+// Make a simple html form that sends a POST request to /login.
 
-func staticHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	path = strings.TrimPrefix(path, "/static")
-	path = filepath.Join("static", path)
-	http.ServeFile(w, r, path)
-}
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prisma Client Go</title>
+</head>
+<body>
+	<form action="/login" method="POST">
+		<input type="email" name="email" placeholder="Email">
+		<input type="password" name="password" placeholder="Password">
+		<button type="submit">Login</button>
+	</form>
+</body>
+</html>
+
