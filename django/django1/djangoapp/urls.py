df958712a974b7b3e6d6622a94df2a7bdc5f7b63
Defy <61808223+De-y@users.noreply.github.com>
2023-04-02 15:21:12 -0700

    I am shifting to Django
    Django is nice,  as well as flask, this is because of how the code structure and routing is. Hard at first, easy looking later on.

---
+++ django/django1/djangoapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include 
+from .views import IndexView, appView
+from django.contrib import admin
+
+urlpatterns = [
+    path('', IndexView.as_view(template_name='index.html'), name='home'),
+    path('app/', appView.as_view(), name='app'),
+    path("accounts/", include("django.contrib.auth.urls")),  # new
+]
