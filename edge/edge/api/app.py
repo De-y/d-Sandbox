5aaeffa8b240383bc01d57f703bd625247182ab9
Defy <61808223+De-y@users.noreply.github.com>
2023-04-04 20:56:40 -0700

    Make the upload function a bit better

--- edge/edge/api/app.py
+++ edge/edge/api/app.py
@@ -3,30 +3,27 @@ import hashlib, os
 
 app = Flask(__name__)
 
-# Route to serve static files from the 'static' directory
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory('/tmp/', filename)
-
-# Create an upload route that upload files from the get request
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload_file():
     if request.method == 'POST':
         try:
-            # Create 'static' directory if it doesn't exist
             if not os.path.exists('/tmp/'):
                 os.makedirs('/tmp/')
             
-            password = 'Welcome to the de-y"s personal CDN' + request.form['password'] + 'aviance SALT.'
+            password = 'TestCDN' + request.form['password'] + 'Welcome to the TestCDN.'
             password = hashlib.sha3_512(password.encode()).hexdigest()
             print(password)
-            if password != '9f82007a073647ef7c36669eafba878cb82f68f285f923f5a3ee22aaf3e524fb8e95f60bf6f11caa846e6ad7ed6e1661a5eca939b02c4e449747b70b47c1c59c':
+            if password != '3367a2008005663d366f6b4d239d0a90417490991c445293061900b851516b43a7d2c5146368a96f052768c0c81353d4aa979d9f39955ee0327c29d124b40c99':
                 return 'Wrong password'
             
             file = request.files['file']
-            file.save('/tmp/' + file.filename)
-            return 'File uploaded successfully, go see it at: <a href="https://de-yscdn.vercel.app/' + file.filename + '">https://de-yscdn.vercel.app/' + file.filename + '</a>'
+            filename = file.filename.replace(' ', '-')
+            file.save('/tmp/' + filename)
+            return 'File uploaded successfully, go see it at: <a href="https://testflaskcdn.vercel.app/' + filename + '">https://testflaskcdn.vercel.app/' + file.filename + '</a>'
         except Exception as e:
             print(e)
             return 'Error uploading file, error:' + str(e)
@@ -38,9 +35,10 @@ def upload_file():
         <body>
         <form action="/upload" method="post" enctype="multipart/form-data">
         <center>
+        <p>Test flask CDN </p><br><br>
         Select file to upload and enter the key:<br><br>
         <input type="file" name="file" id="file"><br><br>
-        <input type="text" value="Password" name="password"><br><br>
+        <input type="text" value="TestPassword" name="password"><br><br>
         <input type="submit" value="Upload" name="submit">
         </center>
         </form>
--- edge/tst.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-password = 'Welcome to the de-y"s personal CDN' + input("password") + 'aviance SALT.'
-password = hashlib.sha3_512(password.encode()).hexdigest()
-print(password)
